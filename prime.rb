f3c61a9e43def5d6db7c61932a372bf1e83b3b73
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-28 17:45:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
-def prime?(integer)
+#def prime?(integer)
   if integer < 2 
     return false
   else 
@@ -15,6 +15,26 @@ def prime?(integer)
   else 
      true
   end
-end 
+#end 
 
+#end
+
+require 'pry'
+
+def prime?(integer)
+  if integer < 2
+    return false
+  else 
+    (2...integer).each do |divisor| 
+      binding.pry
+    if integer % divisor == 0
+    return false
+  end
+  
+  if integer < divisor == 0 
+    return false
+  else 
+     true
+  end
+ end 
 end
