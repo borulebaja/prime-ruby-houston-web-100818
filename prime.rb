99e801503f7f46a3ea6d59b842a40cd37843ed0f
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-28 16:10:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,5 +8,10 @@ def prime?(integer)
 end
 
 def prime?(integer)
- (2...integer).none? {|i| integer % i == 0}
+  (2...integer).each do |divisor| 
+    if integer % divisor == 0
+     return false
+    end
+  end
+  true
 end
