900f0adbfbcaf285ad1553c3e2ff44ed5620447c
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-28 17:43:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,15 @@ def prime?(integer)
   if integer < 2 
     return false
   else 
-    (2...integer).each do |prime?| 
+    (2...integer).each do |divisor| 
     true
   end
-    if integer % prime? == 0
+    if integer % divisor == 0
       return false
     else true
   end
   
-  if integer < prime? == 0 
+  if integer < divisor == 0 
     return false
   else 
      true
